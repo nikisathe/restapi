6c0202097666041826132191850d2fbfaf945c59
nikisathe <nikitasathe1734@gmail.com>
2024-04-27 15:02:09 +0530

    commit

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+const books = [
+  { id: 1, title: 'Book 1', author: 'Author 1' },
+  { id: 2, title: 'Book 2', author: 'Author 2' },
+  { id: 3, title: 'Book 3', author: 'Author 3' }
+];
+
+
+app.get('/api/books', (req, res) => {
+  res.json(books);
+});
+
+
+app.get('/api/books/:id', (req, res) => {
+  const book = books.find(b => b.id === parseInt(req.params.id));
+  if (!book) return res.status(404).send('Book not found');
+  res.json(book);
+});
+
+
+app.post('/api/books', (req, res) => {
+  const book = {
+    id: books.length + 1,
+    title: req.body.title,
+    author: req.body.author
+  };
+  books.push(book);
+  res.json(book);
+});
+
+
+app.put('/api/books/:id', (req, res) => {
+  const book = books.find(b => b.id === parseInt(req.params.id));
+  if (!book) return res.status(404).send('Book not found');
+
+  book.title = req.body.title;
+  book.author = req.body.author;
+
+  res.json(book);
+});
+
+
+app.delete('/api/books/:id', (req, res) => {
+  const bookIndex = books.findIndex(b => b.id === parseInt(req.params.id));
+  if (bookIndex === -1) return res.status(404).send('Book not found');
+
+  books.splice(bookIndex, 1);
+  res.json(books);
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
